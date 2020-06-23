713aa94a378a70976cb271f7fe81748e3d51bd75
Mikhael Korneev <korneevm@gmail.com>
2020-06-23 12:34:02 +0300

    Learn Python Bots 7 code

---
+++ db.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from random import choice
+from emoji import emojize
+from pymongo import MongoClient
+import settings
+
+client = MongoClient(settings.MONGO_LINK)
+
+db = client[settings.MONGO_DB]
+
+
+def get_or_create_user(db, effective_user, chat_id):
+    user = db.users.find_one({"user_id": effective_user.id})
+    if not user:
+        user = {
+            "user_id": effective_user.id,
+            "first_name": effective_user.first_name,
+            "last_name": effective_user.last_name,
+            "username": effective_user.username,
+            "chat_id": chat_id,
+            "emoji": emojize(choice(settings.USER_EMOJI), use_aliases=True)
+        }
+        db.users.insert_one(user)
+    return user
+
+
+def save_anketa(db, user_id, anketa_data):
+    user = db.users.find_one({"user_id": user_id})
+    anketa_data['created'] = datetime.now()
+    if 'anketa' not in user:
+        db.users.update_one(
+            {'_id': user['_id']},
+            {'$set': {'anketa': [anketa_data]}}
+        )
+    else:
+        db.users.update_one(
+            {'_id': user['_id']},
+            {'$push': {'anketa': anketa_data}}
+        )
+
+
+def subscribe_user(db, user_data):
+    if not user_data.get('subscribed'):
+        db.users.update_one(
+            {'_id': user_data['_id']},
+            {'$set': {'subscribed': True}}
+        )
+
+
+def unsubscribe_user(db, user_data):
+    db.users.update_one(
+        {'_id': user_data['_id']},
+        {'$set': {'subscribed': False}}
+    )
+
+
+def get_subscribed(db):
+    return db.users.find({"subscribed": True})
---
+++ utils.py
@@ -0,0 +1,38 @@
+from clarifai.rest import ClarifaiApp
+from random import randint
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+
+import settings
+
+
+def play_random_numbers(user_number):
+    bot_number = randint(user_number - 10, user_number + 10)
+    if user_number > bot_number:
+        message = f"Ваше число {user_number}, мое {bot_number}, вы выиграли"
+    elif user_number == bot_number:
+        message = f"Ваше число {user_number}, мое {bot_number}, ничья"
+    else:
+        message = f"Ваше число {user_number}, мое {bot_number}, вы проиграли"
+    return message
+
+
+def main_keyboard():
+    return ReplyKeyboardMarkup([
+        ['Прислать котика', KeyboardButton('Мои координаты', request_location=True), 'Заполнить анкету']
+    ])
+
+
+def is_cat(file_name):
+    app = ClarifaiApp(api_key=settings.CLARIFAI_API_KEY)
+    model = app.public_models.general_model
+    responce = model.predict_by_filename(file_name, max_concepts=5)
+    if responce['status']['code'] == 10000:
+        for concept in responce['outputs'][0]['data']['concepts']:
+            if concept['name'] == 'cat':
+                return True
+    return False
+
+
+if __name__ == "__main__":
+    print(is_cat("images/cat1.jpg"))
+    print(is_cat("images/not_cat.jpg"))
